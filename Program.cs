bbd2ad4829c7231c730bb3291d873f1ad09b8cd9
Folasayo Samuel <folasayosamuel@gmail.com>
2022-10-10 11:23:26 +0100

    update

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LibraryFineCalculationApp
+{
+	public class Program
+	{
+		public static void Main(string[]args)
+		{
+			int days;
+			float fine = 0;
+			Console.Write("Enter total days: ");
+			days = Convert.ToInt32(Console.ReadLine());
+			
+			if(days <= 5)
+			{
+				fine = 0;
+			} else if(days > 5 && days <= 10)
+			{
+				fine = (days - 5) * 0.5F;
+			} else if(days > 10 && days <= 30)
+			{
+				//  ----- 5 days -----   --- between 10 and 30 ---
+				fine = 5 * 0.5F + (days - 10) * 1;
+			} else
+			{
+				//  --- 5 days --- ---10, 30--- 
+				fine = 5 * 0.5F + 20 * 1 + (days - 30) * 1.5F;
+				System.Console.WriteLine("Canceled your Membership");
+			}
+			
+			Console.WriteLine("Your fine: "+fine);
+
+            Console.ReadLine();
+
+        }
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Program-Library-Fine-Calculation-in-C-Sharp
+
+# How the program work:
+## The fee structure is as follows:
+
+### 1. If the book is returned on  before  5 days, no fine will be charged.
+### 2. If the book is returned after the expected return day (between 5 and 10 days) – fine: 0.5$ per day.
+### 3. If the book is returned after the expected return day (between 10 and 30 days) fine: 1$ per day.
+### 4. If the book is not returned after 30 days, cancel membership. fine: 1.5$ per day.
